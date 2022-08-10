fd03b89e0912dd6267706fac6e68af53b5e33c5a
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-08-09 21:03:11 -0400

    yep

--- views/index.jsx
+++ views/index.jsx
@@ -1,11 +1,21 @@
 const React = require('react')
 const Default = require('./layouts/defualt')
 
-function Index () {
+function Index({ breads }) {
     return (
-      <Default>
-        <h2>Index Page</h2>
-      </Default>
+        <Default>
+            <h2>Index Page</h2>
+            <ul>{
+                breads.map((bread, index) => {
+                    return (
+                        <li key={index}>
+                            {bread.name}
+                        </li>
+                    )
+                })
+            }
+            </ul>
+        </Default>
     )
 }
 
