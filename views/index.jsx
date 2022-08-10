d1d62b975a6f962baa75b302944f0c92db827521
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-08-09 20:38:24 -0400

    Rendering

---
+++ views/index.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const Default = require('./layouts/defualt')
+
+function Index () {
+    return (
+      <Default>
+        <h2>Index Page</h2>
+      </Default>
+    )
+}
+
+module.exports = Index
