59ab1a07d1ff48c94de8d34d4c612e64740ddb79
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-08-18 20:37:59 -0400

    mongodb

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+const mongoose = require('mongoose')
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
@@ -13,6 +14,10 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(methodOverride('_method'))
+mongoose.connect(process.env.MONGO_URI, {family:4}, function(err, connection) {
+  // connection.db('breads');
+  console.log('connected to DB!!!!!!!!')
+});
 
 // ROUTES
 app.get('/', (req, res) => {
