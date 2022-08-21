1ac9a9d65674240c760bb25a28782189b152f88b
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-08-21 11:01:59 -0400

    delete/edit button

--- views/show.jsx
+++ views/show.jsx
@@ -7,7 +7,10 @@ function Show({ bread, index }) {
     return (
         <Default>
             <h3>{bread.name}</h3>
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+            <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
+                <input type='submit' value="DELETE" />
+            </form>
             <p>
                 and it
                 {
@@ -20,7 +23,7 @@ function Show({ bread, index }) {
             <img src={bread.image} alt={bread.name} />
             <li><a href="/breads">Go home</a></li>
         </Default>
-        )
+    )
 
 }
 
