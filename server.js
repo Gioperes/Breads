34cca0ef3816f390d22de233459bdb8235747d81
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-08-23 19:42:55 -0400

    bakers

--- server.js
+++ server.js
@@ -24,14 +24,19 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
   
-  // Breads
-  const breadsController = require('./controllers/breads_controller')
-  app.use('/breads', breadsController)
-  
-  // 404 Page
+  // breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+// bakers 
+const bakersController = require('./controllers/bakers_controller.js')
+app.use('/bakers', bakersController)
+
+// 404 Page
 app.get('*', (req, res) => {
   res.send('404')
 })
+
 
 // LISTEN
 app.listen(PORT, () => {
